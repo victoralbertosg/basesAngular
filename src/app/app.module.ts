201b35e688c489b346c5dba2f1cea068903fa926
victoralbertosg <undacvasg@hotmail.com>
2021-03-19 00:17:30 -0500

    actualizacion 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { HeroesModule } from './heroes/heroes.module';
 
@@ -14,10 +15,12 @@ import { HeroesModule } from './heroes/heroes.module';
     
   
   ],
+ 
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
